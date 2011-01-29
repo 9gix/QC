f7b9db49a0d0b35e9753ccbb5fa2ca597405ea76
Eugene <yeo.eugene.oey@gmail.com>
2011-01-29 12:07:50 +0800

    Contrasting BlackWhite

--- main.py
+++ main.py
@@ -10,22 +10,65 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 import Image
+import ImageChops
+import math, operator
 
 def main():
     # m1 == m3 # m1 != m2
     originalImageFile = "images\m1.jpg"
     ori = getImage(originalImageFile)
-    img = getImage("images\m3.jpg")
+    img = getImage("images\m2.jpg")
     samePercentage = compare(ori,img)
     saveImage(img)
 
 def compare(ori,img):
-    ori = list(ori.getdata())
-    img = list(img.getdata())
-    print ori==img
+    #ori = list(ori.getdata())
+    #img = list(img.getdata())
+    #img = repositionImage(ori,img)
+    return img
+
+def equal(im1, im2):
+    return ImageChops.difference(im1, im2).getbbox() is None
+
+def rmsdiff(im1, im2):
+    "Calculate the root-mean-square difference between two images"
+
+    h = ImageChops.difference(im1, im2).histogram()
+
+    # calculate rms
+    return math.sqrt(reduce(operator.add,
+        map(lambda h, i: h*(i**2), h, range(256))
+    ) / (float(im1.size[0]) * im1.size[1]))
+
+def repositionImage(ori,img):
+    """
+    for each pixel in img:
+        for eac
+    """
+    return img
 
 def getImage(fp):
-    return grayscale(Image.open(fp))
+    img = grayscale(Image.open(fp))
+    #img = img.point(lambda i: i * 0.2)
+    x,y = img.size
+    lvl = 90
+    source = img.split()
+    # select regions where color is less than lvl
+    mask = source[0].point(lambda i: i <= lvl and 255)
+    # process the green band
+    out = source[0].point(lambda i: i * 0.0)
+    # paste the processed band back, but only where color was < 105
+    source[0].paste(out, None, mask)
+    img = Image.merge(img.mode, source)
+    # select regions where color is less than lvl
+    mask = source[0].point(lambda i: i > lvl and 255)
+    # process the green band
+    out = source[0].point(lambda i: i * 100.0)
+    # paste the processed band back, but only where color was < 105
+    source[0].paste(out, None, mask)
+    # build a new multiband image
+    img = Image.merge(img.mode, source)
+    return img
 
 def grayscale(img):
     return img.convert("L")
